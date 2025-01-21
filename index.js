ebb405567529227fff22fc4e2d3f0f5e1d23ce18
Nikolay Komarov <kny.komarov@gmail.com>
2025-01-21 15:23:05 +0300

    добавит простой сервер на nodejs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('init');
+import http from 'http';
+
+const host = 'localhost';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
+});
+
+server.listen(port, host, () => {
+  console.log(`Listening on port ${port}`);
+});
