f4a3db8cbbe32fba75d9c447cf823b610139f628
Nikolay Komarov <kny.komarov@gmail.com>
2025-01-22 17:16:56 +0300

    добавит middlewares (log, err)

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ import { usersRouter } from "./users/users.js";
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('logTime: ', Date.now());
+  next();
+})
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+app.get('/hello', (req, res) => {
+  throw new Error('error');
+})
+
 app.use('/users', usersRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(401).send(err.message);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
